5ce1f108197eeec944844f39d78dae86b7c8ced1
Aykut Merdivan <merdivanaykut@gmail.com>
2023-04-15 02:04:53 +0300

    Konum geçmişi görüntüleme eklendi.

--- pages/konum.vue
+++ pages/konum.vue
@@ -2,14 +2,19 @@
   <div class="container">
     <menulist />
     <div class="content">
-      <h1>Bildirim Gönder</h1>
+      <h1>Konum geçmişi görüntüle</h1>
       <div id="map"></div>
     </div>
     <oturumacik />
     <footerkismi />
   </div>
 </template>
+
 <script>
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+
 import footerkismi from "~/components/footer-kismi.vue";
 import menulist from "~/components/menu-list.vue";
 import oturumacik from "~/components/oturum-acik.vue";
@@ -20,43 +25,68 @@ export default {
     oturumacik,
     footerkismi,
   },
+  data() {
+    return {
+      user: '',
+      markers: [],
+    }
+  },
   mounted() {
-    // Google Maps API'ye yükleme yapılır
+    this.oturum()
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=API_KEY`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`;
     script.defer = true;
     script.async = true;
-    script.onload = this.initMap;
+    script.onload = this.mapYukle;
     document.head.appendChild(script);
   },
   methods: {
-    initMap() {
-      // Harita özellikleri belirlenir
-      const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.4219983, lng: -122.084 },
+    oturum(){
+      firebase.auth().onAuthStateChanged(user=>{
+        console.log(user)
+        this.user = user;
+        this.markerGetir();
+      })
+    },
+    
+    async markerGetir() {
+      const snapshot = await firebase.firestore()
+        .collection('logs')
+        .doc(this.user.uid)
+        .collection('konumlar')
+        .get();
+
+      snapshot.forEach(doc => {
+        const data = doc.data();
+        const marker = new google.maps.Marker({
+          position: { lat: data.latitude, lng: data.longitude },
+          map: this.map,
+          title: `${new Date(data.tarih).toLocaleDateString()} ${new Date(data.tarih).toLocaleTimeString()}`,
+        });
+
+        marker.addListener("click", () => {
+          const infowindow = new google.maps.InfoWindow({
+            content: `${new Date(data.tarih).toLocaleDateString()} ${new Date(data.tarih).toLocaleTimeString()}`,
+          });
+          infowindow.open(this.map, marker);
+        });
+
+        this.markers.push(marker);
+      });
+    },
+    mapYukle() {
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: this.markers.length > 0 ? this.markers[0].getPosition().lat() : 41.0174, lng: this.markers.length > 0 ? this.markers[0].getPosition().lng() : 28.9883 },
         zoom: 8,
       });
 
-      // Marker oluşturulur
-      const marker = new google.maps.Marker({
-        position: { lat: 37.4219983, lng: -122.084 },
-        map: map,
-        title: "Konum",
-      });
-
-      // InfoWindow oluşturulur
-      const infowindow = new google.maps.InfoWindow({
-        content: "Konum",
-      });
-
-      // Marker'a tıklandığında InfoWindow açılır
-      marker.addListener("click", () => {
-        infowindow.open(map, marker);
-      });
+      this.markerGetir();
     },
   },
 };
 </script>
+
+
 <style>
 #map {
   height: 400px;
